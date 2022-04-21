bae386e96a34baded47c50bfeedf08421895e301
Alba Moreno <moreno.alba.97@gmail.com>
2022-04-21 17:23:33 +0200

    Se añade el archivo StoreCurso.php para crear validaciones complejas Se modifica CursoController.php para usar las validaciones de StoreCurso.php

--- app/Http/Controllers/CursoController.php
+++ app/Http/Controllers/CursoController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Curso;
 use Illuminate\Http\Request;
+use App\Http\Requests\StoreCurso;
 
 class CursoController extends Controller
 {
@@ -19,13 +20,7 @@ class CursoController extends Controller
         return view('cursos.create');
     }
 
-    public function store(Request $request){
-
-        $request->validate([
-            'name' => 'required|max:10',
-            'descripcion' => 'required|min:10',
-            'categoria' => 'required'
-        ]);
+    public function store(StoreCurso $request){
         
         $curso = new Curso();
 
