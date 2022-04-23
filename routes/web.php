9872d970f9cbe247cad26a77844931bc787b1009
Alba Moreno <moreno.alba.97@gmail.com>
2022-04-23 13:33:15 +0200

    Se crea la lógica para un formulario de contacto

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,11 @@
 <?php
 
+use App\Http\Controllers\ContactanosController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\CursoController;
 
-use App\Mail\ContactanosMailable;
-use Illuminate\Support\Facades\Mail;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -55,9 +55,6 @@ Route::resource('cursos', CursoController::class);
 
 Route::view('nosotros', 'nosotros')->name('nosotros');
 
-Route::get('contactanos', function(){
-    $correo = new ContactanosMailable;
-    Mail::to('moreno.alba.97@gmail.com')->send($correo);
+Route::get('contactanos', [ContactanosController::class, 'index'])->name('contactanos.index');
 
-    return "Mensaje enviado";
-});
+Route::post('contactanos', [ContactanosController::class, 'store'])->name('contactanos.store');
